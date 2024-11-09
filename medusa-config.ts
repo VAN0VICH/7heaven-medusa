aeb5e5627a7102d78e022955120f80128cb46a21
LordPachimari <opachimari@gmail.com>
2024-11-10 01:44:34 +0300

    redis add

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,7 +12,8 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
-    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server"
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    redisUrl:process.env.REDIS_URL!
   },
   admin: {
     disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
